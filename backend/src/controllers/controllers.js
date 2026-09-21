6b25a004093b313c3a469991a582217e5f133f35
alirazamirbahar254-dev <alirazamirbahar254-dev@gmail.com>
2026-09-21 23:16:21 +0500

    feat: protect task update by ownership

--- backend/src/controllers/controllers.js
+++ backend/src/controllers/controllers.js
@@ -50,7 +50,7 @@ try{
     const { id } = req.params;
 
     const result = await pool.query(`UPDATE tasks 
-    SET title = $1, description = $2, columnid = $3, completed = $4 WHERE id = $5 RETURNING*`,               [title,description,columnid,completed,id]); 
+    SET title = $1, description = $2, columnid = $3, completed = $4 WHERE id = $5 and user_id = $6  RETURNING*`,       [title,description,columnid,completed,id, req.loginuser.user_id]); 
     res.status(200).json(result.rows);
 }catch (error) {
     console.error(error);
