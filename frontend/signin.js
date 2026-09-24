36db6616c150cd7e8018122c9e087ea7b185b0e0
alirazamirbahar254-dev <alirazamirbahar254-dev@gmail.com>
2026-09-24 23:26:05 +0500

    feat: connect signin form to login API

---
+++ frontend/signin.js
@@ -0,0 +1,42 @@
+const signin = document.querySelector("#signin");
+const login = document.querySelector("#login");
+
+
+console.log("signin.js loaded");
+login.addEventListener("click", async () =>{
+
+const email = document.querySelector("#email").value;
+const password = document.querySelector("#password").value;
+try {
+    
+const userdata = {email:email, password:password}
+console.log("before fetch");
+const response = await fetch(`http://localhost:3000/auth/login`,{
+
+method : "POST",
+
+headers : {
+    "content-type" : "application/JSON",
+},
+
+body : JSON.stringify(userdata)
+});
+console.log(response.status);
+const data = await response.json()
+
+//redirect to task manager after login
+if(response.status === 200){
+console.log("Login successful")
+window.location = "index.html"
+}
+
+localStorage.setItem("token",data.token)
+console.log(data)
+
+}
+catch(error) {
+   console.log(error)
+}
+})
+
+
